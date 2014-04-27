ccc2afb6a8e1efd9ba9f0018b2af8977586f87a6
Johan Ronström <johan@johanronstrom.se>
2014-04-27 21:27:31 +0200

    Theme and locale fixes

---
+++ forum/locales/sthlmesport/definitions.php
@@ -0,0 +1,10 @@
+<?php
+
+ $LocaleInfo['sthlmesport'] = array (
+  'Locale' => 'sthlmesport',
+  'Name' => 'STHLM e-sport. Baserad på Swedish Transifex',
+  'Description' => 'STHLM e-sport. Based on Swedish language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/sv_SE/">here</a>.',
+  'Version' => '2014.04.11',
+  'Author' => 'Johan Ronström, Vanilla Community',
+  'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/sv_SE/',
+);
--- forum/themes/sthlmesport/views/default.master.php
+++ forum/themes/sthlmesport/views/default.master.php
@@ -17,12 +17,20 @@
 			</div>
 
 			<nav role="navigation" class="main-navigation" id="site-navigation">
-				<div class="menu"><ul><li><a href="http://sthlmesport.se/">Home</a></li><li class="page_item page-item-2075"><a href="http://sthlmesport.se/kalender/">Kalender</a></li><li class="page_item page-item-1486"><a href="http://sthlmesport.se/om-sthlm-e-sport/">Om STHLM e-sport</a></li></ul></div>
+				<div class="menu"><ul><li><a href="http://sthlmesport.se/">Home</a></li><li class="page_item page-item-2075"><a href="http://sthlmesport.se/kalender/">Kalender</a></li><li class="page_item page-item-1486"><a href="http://sthlmesport.se/om-sthlm-e-sport/">Om STHLM e-sport</a></li><li class="page_item"><a href="http://sthlmesport.se/forum/">Forum</a></li></ul></div>
 			</nav><!-- #site-navigation -->
 		</header>
 
 
-         <div id="Content"><h2>Forum</h2><?php $this->RenderAsset('Content'); ?></div>
+         <div id="Content">
+         	<a href="http://sthlmesport.se/forum/?ShowAllCategories=true"><h2>Forum</h2></a>
+
+         	<div class="BreadcrumbsTab"><?php echo Gdn_Theme::Breadcrumbs( $this->Data('Breadcrumbs') ) ?></div>
+
+         	<?php $this->RenderAsset('Content'); ?>
+
+         </div>
+         
          <div id="Panel">
 
 				<?php
